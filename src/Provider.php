52d7ce3b743f2e0754772219abd3e1d516073d7f
DraperStudio <hello@draperstudio.tech>
2016-04-15 12:23:37 +0200

    Add mapUserToObject method

--- src/Provider.php
+++ src/Provider.php
@@ -15,18 +15,12 @@ class Provider extends AbstractProvider
     /**
      * {@inheritdoc}
      */
-    public function user()
+    protected function mapUserToObject(array $user)
     {
-        if (!$this->hasNecessaryVerifier()) {
-            throw new \InvalidArgumentException('Invalid request. Missing OAuth verifier.');
-        }
-
-        $user = $this->server->getUserDetails($token = $this->getToken());
-
         return (new User())->setRaw($user->extra)->map([
             'id' => $user['key'], 'nickname' => $user['name'],
             'name' => $user['name'], 'email' => $user['email'],
             'avatar' => array_get($user, 'avatar'),
-        ])->setToken($token->getIdentifier(), $token->getSecret());
+        ]);
     }
 }
